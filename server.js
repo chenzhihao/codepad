4c26b89aff5d269fd0e7a56f9766176ef6bf4465
chenzhihao <cabtravis@gmail.com>
2017-01-18 17:45:11 +0800

    Add express dependencies

--- server.js
+++ server.js
@@ -2,13 +2,34 @@ const express = require('express');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dir: '.', dev });
+const app = next({dir: '.', dev});
 const handle = app.getRequestHandler();
+const morgan = require('morgan');
+const path = require('path');
+const _ = require('lodash');
+const cors = require('cors');
+const methodOverride = require('method-override');
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const router = require('./router');
 
 app.prepare()
   .then(() => {
     const server = express();
+
+    server.use(morgan('dev'));
+    server.use(cors());
+    server.options('*', cors());
+
+    server.use(compression());
+
+    server.use(bodyParser.urlencoded({extended: false}));
+    server.use(bodyParser.json());
+
+    server.use(cookieParser());
+    server.use(methodOverride());
 
     server.use(router);
 
